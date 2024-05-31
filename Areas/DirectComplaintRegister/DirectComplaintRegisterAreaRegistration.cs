3db33e4b681d74c3dbf0270f651034e5f60c34ab
ajayPrismHR <107325460+ajayPrismHR@users.noreply.github.com>
2024-05-31 08:36:56 +0530

    Drop files from .gitignore

--- Areas/DirectComplaintRegister/DirectComplaintRegisterAreaRegistration.cs
+++ Areas/DirectComplaintRegister/DirectComplaintRegisterAreaRegistration.cs
@@ -19,15 +19,6 @@ namespace ComplaintTracker.Areas.DirectComplaintRegister
                 "DirectComplaint/{controller}/{action}/{id}",
                 new { action = "Index", id = UrlParameter.Optional }
             );
-
-            
-
-            context.MapRoute(
-                "CloseComplaint",
-                "CloseComplaint/{controller}/{action}/{id}",
-                new { action = "closeSearch", id = UrlParameter.Optional }
-            );
-
             
         }
     }
